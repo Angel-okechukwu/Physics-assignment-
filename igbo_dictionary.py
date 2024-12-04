838b5454316416dc7cc6e6a4f9ace38239719a2d
Angel-okechukwu <angel.okechukwu@binghamuni.edu.ng>
2024-12-04 14:47:55 +0100

    Add files via upload
    word = input('enter an igbo word')
    print(igbo_dictionary[word])

---
+++ igbo_dictionary.py
@@ -0,0 +1,13 @@
+igbo_dictionary = {'bia' : come,
+'ogini' : what is it?,
+'were' : take,
+'neba' : here,
+'ego' : money,
+'biko' : please,
+'ifunanya' : love,
+'afa' : name,
+'akwa' : egg,
+'inri' : food
+}
+word = input('enter an igbo word')
+print(igbo_dictionary[word]
